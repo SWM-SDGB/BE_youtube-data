5da2515523eed63cba40769ff964664c24efe943
GeonHyeock <gunheock@gmail.com>
2024-06-13 18:38:57 +0900

    Refactor : repackaging

--- parallelism/util/youtube_livechat_crawling_nonBuffer.py
+++ parallelism/util/youtube_livechat_crawling_nonBuffer.py
@@ -1,6 +1,6 @@
 import pytchat
 import pandas as pd
-
+from collections import defaultdict
 from exception.EmptyLiveChatException import EmptyLiveChatException
 from exception.LiveChatUnknownException import LiveChatUnknownException
 
@@ -8,19 +8,18 @@ from exception.LiveChatUnknownException import LiveChatUnknownException
 async def live_chat(videoId, folder):
     chat = pytchat.create(video_id=videoId)
     file_path = f"./{folder}/{videoId}.csv"
-
+    data = defaultdict(list)
     while chat.is_alive():
         chatdata = chat.get()
         for item in chatdata.items:
-            data = {
-                "ID": [item.id],
-                "Name": [item.author.name],
-                "comment": [item.message],
-                "Date": [item.datetime],
-                "time": [item.elapsedTime],
-            }
-            result = pd.DataFrame(data)
-            result.to_csv(file_path, mode="a", index=False)
+            data["ID"].append(item.id)
+            data["Name"].append(item.author.name)
+            data["comment"].append(item.message)
+            data["Date"].append(item.datetime)
+            data["time"].append(item.elapsedTime)
+
+    result = pd.DataFrame(data)
+    result.to_csv(file_path, mode="a", index=False)
 
     try:
         # 채팅이 더이상 존재하지 않은면 exception 발생
