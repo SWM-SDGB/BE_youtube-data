28ba2b0d76ffd4164f7190f8e38d7cf7713df929
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-06-22 11:25:34 +0900

    Feat: 비디오 수집 날짜 - summary.csv #22

--- parallelism/util/summary_generate.py
+++ parallelism/util/summary_generate.py
@@ -1,7 +1,12 @@
+from datetime import datetime
 
 def get_video_name(driver):
   video_name = driver.title.split('-')[0]
   print(video_name)
   return video_name
+
+def get_collection_date():
+  now = datetime.now()
+  return now.strftime("%Y/%m/%d")
 
 
