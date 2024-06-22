766f9d4339b59a7214ba102c59e31e9fc34f9a03
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-06-22 11:57:00 +0900

    Feat: 비디오 조회수 수집 - summary.csv #22

--- parallelism/util/summary_generate.py
+++ parallelism/util/summary_generate.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import re
 
 def get_video_name(driver):
   video_name = driver.title.split('-')[0]
@@ -9,4 +10,13 @@ def get_collection_date():
   now = datetime.now()
   return now.strftime("%Y/%m/%d")
 
-
+def get_view_score(description_inner_text):
+  pattern = r'조회수 (\d{1,3}(,\d{3})*)회'
+  match = re.search(pattern, description_inner_text)
+  if match:
+    # 첫 번째 그룹 (조회수 숫자)
+    views = match.group(1)
+    return views
+  else:
+    print("조회수를 찾을 수 없습니다.")
+    return
