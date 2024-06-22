db1ffc09190e4ffa3ebcc286fbc6aa2dfd64c547
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-06-22 17:06:31 +0900

    Refactor: 유튜브 라이브 목록 수집 출력 텍스트 변경 #24

--- parallelism/util/youtube_live_video_list_crawling.py
+++ parallelism/util/youtube_live_video_list_crawling.py
@@ -13,6 +13,7 @@ SCROLL_DOWN_WAIT = 1
 
 
 def get_video_urls_by_selenium(channel_id):
+  print("[셀레니움] 채널의 라이브영상 목록 수집 시작")
   # 백그라운드 실행 옵션 추가
   options = webdriver.ChromeOptions()
   options.add_argument("headless")
@@ -32,7 +33,6 @@ def url_by_channel_id(channel_id):
   return 'https://www.youtube.com/'+channel_id+'/streams'
 
 def scroll_down(driver):
-  print("start - scroll down")
 
   while True:
     num_items = len(driver.find_elements(By.CSS_SELECTOR, "div[id='content'] div[class='style-scope ytd-rich-item-renderer']"))
